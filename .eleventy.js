a724dd8b58f69140e6edb73857a919c546e7441e
Paul Shryock <paul@pshry.com>
2019-02-09 11:11:00 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+module.exports = (function(eleventyConfig) {
+
+	const assets = [
+		'css',
+	];
+
+	assets.forEach((asset) => {
+	  try {
+		  if (fs.existsSync(`./src/${asset}`)) {
+				eleventyConfig.addPassthroughCopy(`src/${asset}`);
+		  }
+		} catch(err) {
+		  console.error(err)
+		}
+	});
+
+	return {
+		passthroughFileCopy: true,
+		dir: {
+			input: "src",
+			output: "_site"
+		}
+	};
+
+});
